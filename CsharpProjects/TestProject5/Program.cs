a9f0cd259f53b5492c8d3964d01df63f6ea772ad
Mxolisi <mxolisibhotile58@gmail.com>
2026-05-05 10:54:16 +0200

    .

---
+++ CsharpProjects/TestProject5/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] fraudulentOrderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235","B179" ];
+
+string fradulentIDs = "";
+
+foreach (string id in fraudulentOrderIDs )
+{
+    if ( id.StartsWith("B") )
+    {
+        fradulentIDs += id;
+        Console.WriteLine($"{id} might be fraudulent it starts with B");
+    }
+}
