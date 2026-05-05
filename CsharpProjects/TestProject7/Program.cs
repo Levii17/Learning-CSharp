258de59a443351673ec122201828e15946eed753
Mxolisi <mxolisibhotile58@gmail.com>
2026-05-05 11:15:06 +0200

    proj 7.

---
+++ CsharpProjects/TestProject7/Program.cs
@@ -0,0 +1,107 @@
+﻿/* 
+Code challenge: rewrite if-elseif-else using a switch statement
+
+You'll start with code that uses an if-elseif-else construct to evaluate the components of a product SKU. The SKU (Stock Keeping Unit) is formatted using three coded values: <product #>-<2-letter color code>-<size code>. For example, a SKU value of 01-MN-L corresponds to (sweat shirt)-(maroon)-(large), and the code outputs a description that appears as "Product: Large Maroon Sweat shirt".
+
+Your challenge is to convert the if statement code to a switch statement that achieves the same result as the initial code.
+*/
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+
+string sku = "02-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+
+/* 
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+} else if (product[0] == "02")
+{
+    type = "T-Shirt";
+} else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+} else if (product[1] == "MN")
+{
+    color = "Maroon";
+} else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+} else if (product[2] == "M")
+{
+    size = "Medium";
+} else if (product[2] == "L")
+{
+    size = "Large";
+} else
+{
+    size = "One Size Fits All";
+}
+*/
+switch (product[0])
+{
+    case "01": 
+        type = "Sweat shirt";
+        break;
+    case "02": 
+        type = "T-Shirt";
+        break;
+    case "03": 
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "small";
+        break;
+    case "M":
+        size = "medium";
+        break;
+    case "L":
+        size = "large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
