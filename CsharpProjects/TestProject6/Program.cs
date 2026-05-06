306296269b844cde4a9427e53fa4e24412533856
Mxolisi <mxolisibhotile58@gmail.com>
2026-05-06 08:58:21 +0200

    .

--- CsharpProjects/TestProject6/Program.cs
+++ CsharpProjects/TestProject6/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Role playing game battle challenge
+
+In some role playing games, the player's character battles non-player characters, which are usually monsters or the "bad guys". Sometimes, a battle consists of each character generating a random value using dice and that value is subtracted from the opponent's health score. Once either character's health reaches zero, they lose the game.
+
+In this challenge, we boil down that interaction to its essence. A hero and a monster start with the same health score. During the hero's turn, they generate a random value that is subtracted from the monster's health. If the monster's health is greater than zero, they take their turn and attack the hero. As long as both the hero and the monster have health greater than zero, the battle resumes.
+Code challenge - write code to implement the game rules
+
+Here are the rules for the battle game that you need to implement in your code project:
+
+    You must use either the do-while statement or the while statement as an outer game loop.
+    The hero and the monster start with 10 health points.
+    All attacks are a value between 1 and 10.
+    The hero attacks first.
+    Print the amount of health the monster lost and their remaining health.
+    If the monster's health is greater than 0, it can attack the hero.
+    Print the amount of health the hero lost and their remaining health.
+    Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+    Print the winner.
+
+*/
+
+int heroHealth = 10;
+int monsterHealth = 10;
+Random random = new Random();
+
+while (heroHealth > 0 && monsterHealth > 0)
+{
+    // Hero attacks
+    int heroAttack = random.Next(1, 11); // Generates a random value between 1 and 10
+    monsterHealth -= heroAttack; // Subtract the attack value from the monster's health
+    Console.WriteLine($"Hero attacks! Monster loses {heroAttack} health and has {monsterHealth} health remaining.");
+
+    // Check if the monster is still alive
+    if (monsterHealth > 0)
+    {
+        // Monster attacks
+        int monsterAttack = random.Next(1, 11); // Generates a random value between 1 and 10
+        heroHealth -= monsterAttack; // Subtract the attack value from the hero's health
+        Console.WriteLine($"Monster attacks! Hero loses {monsterAttack} health and has {heroHealth} health remaining.");
+    } else
+    {
+        Console.WriteLine("Monster has been defeated! Hero wins!");
+        break; // Exit the loop if the monster is defeated
+    }
+}
